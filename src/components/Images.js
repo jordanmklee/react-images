a76a2d7891e83bfe89cc290ab6af4fd6b527bd41
Jordan <jordan.mk.lee@gmail.com>
2021-06-07 15:35:25 -0600

    Added buttons, styling

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from '@material-ui/icons/Search';
+import Button from "@material-ui/core/Button";
 
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
@@ -10,41 +11,54 @@ import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 import axios from "axios";
-const URL = "https://tsmiscwebapi.azurewebsites.net/api/qb/GetSBSIFPictures/1/10000/1";
+const API_GET_PICTURES = "https://tsmiscwebapi.azurewebsites.net/api/qb/GetSBSIFPictures/";
 
 function Images(){
     const [images, setImages] = useState([]);
     
     const getImages = (id) => {
-        axios.get(URL)
+        axios.get(API_GET_PICTURES
+                    + "/" + id
+                    + "/10000/1")
             .then(res => {
                 let apiImages = [];
 
                 res.data.Data.forEach(img => {
                     apiImages.push({
-                        orignal: img.ImageUrl,
+                        original: img.ImageUrl,
+                        originalHeight: 400,
+
                         thumbnail: img.ThumbImageUrl,
+                        thumbnailHeight: 100,
+                        thumbnailWidth: 100,
                     })
                 })
 
                 setImages(apiImages);
             })
-    }
-
+    }        
+    
     useEffect(() => {
-        getImages();
+        getImages(2);
     }, [])
 
     return(
         <>
-            <AppBar position="static">
-                <Toolbar>
-                    <h2>Items</h2>
-                </Toolbar>
-            </AppBar>
+            <Container>
+                <h1>Images</h1>
+            </Container>
 
-            <Container><Paper>
+            <Container className="baseContainer" style={{ textAlign: "center" }}><Paper>
+                <div className="inputContainer" style={{ alignItems: "center" }}>
+                    <TextField variant="outlined" size="small" label="Search by ID"></TextField>
+                    <Button variant="contained" color="primary"><SearchIcon/></Button>
+                </div>
+            </Paper></Container>
+
+            <Container className="baseContainer"><Paper>
+                <div style={{ padding: 30 }}>
                 <ImageGallery items={images}/>
+                </div>
             </Paper></Container>
         </>
     )
