681d3276822f85b38d3ea8026e83ba53d640c932
Jordan <jordan.mk.lee@gmail.com>
2021-06-08 11:12:37 -0600

    Changed req, don't need search by ID (done via URL)

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import Select from '@material-ui/core/Select';
-import MenuItem from "@material-ui/core/MenuItem";
-import Button from "@material-ui/core/Button";
-import SearchIcon from '@material-ui/icons/Search';
 
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
@@ -14,16 +9,18 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import Gallery from "react-grid-gallery";
 
 import axios from "axios";
-const API_GET_SBS = "https://tsmiscwebapi.azurewebsites.net/api/qb/GetSBSIFPictures/";
-const API_GET_TST = "https://tsmiscwebapi.azurewebsites.net/api/timesheet/GetTimesheetPictures/";
+const API_GET_SBS = "https://tsmiscwebapi.azurewebsites.net/api/qb/GetSBSIFPictures";
+const API_GET_TST = "https://tsmiscwebapi.azurewebsites.net/api/timesheet/GetTimesheetPictures";
 
 function Images(){
 	const [images, setImages] = useState([]);
-	const [searchId, setSearchId] = useState("");
-	const [type, setType] = useState("SBS");
 	
 	// Retrieves pictures from API and updates images state array
-	const updateImages = (id) => {
+	useEffect(() => {
+		let search = window.location.search;
+		let type = search.substring(5, 8);
+		let id = search.substring(12);
+
 		axios.get((type === "SBS" ? API_GET_SBS : API_GET_TST)
 		+ "/" + id
 		+ "/10000/1")
@@ -45,45 +42,14 @@ function Images(){
 			
 			setImages(apiImages);
 		})
-	}        
-	
-	const handleSearchInput = (event) => {
-		setSearchId(event.target.value);
-	}
-	
-	const handleChangeType = (event) => {
-		setType(event.target.value);
-	}
-	
-	const handleSearchClick = (event) => {
-		updateImages(searchId);
-	}
+
+	}, [])
 	
 	return(
 		<>
 			<Container className="baseContainer" maxWidth={false}>
 				<h1>Images</h1>
 			</Container>
-
-			<Container className="baseContainer" style={{ textAlign: "center" }} maxWidth={false}><Paper>
-				<div className="inputContainer" style={{ padding: 20 }}>
-					<TextField variant="outlined"
-						label="Search by ID"
-						onChange={handleSearchInput}/>
-					
-					<Select variant="outlined"
-						value={type}
-						onChange={handleChangeType}>
-						<MenuItem value="SBS">SBS</MenuItem>
-						<MenuItem value="TST">TST</MenuItem>
-					</Select>
-					
-					<Button variant="contained" color="primary" size="large"
-						onClick={handleSearchClick}>
-						<SearchIcon/>
-					</Button>
-				</div>
-			</Paper></Container>
 
 			{/* Only show gallery if there are images */}
 			{(images.length !== 0)
